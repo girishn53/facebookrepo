5af633de7eba3649c66d62c4c4c00c3429a1cff3
ab48044 <ab48044@XIN009ONAB48044.ctl.intranet>
2016-06-23 15:57:15 +0530

    commited files on 23/06/2016

--- src/test/java/com/ctliselenium/framework/datadriven/Billing/PaymentOptionsTest.java
+++ src/test/java/com/ctliselenium/framework/datadriven/Billing/PaymentOptionsTest.java
@@ -5,11 +5,13 @@ import java.util.Hashtable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.ctliselenium.framework.datadriven.TestBase;
 import com.ctliselenium.framework.datadriven.util.Constants;
+import com.ctliselenium.framework.datadriven.util.ErrorUtil;
 import com.ctliselenium.framework.datadriven.util.TestDataProvider;
 
 public class PaymentOptionsTest extends TestBase {
@@ -31,14 +33,18 @@ public class PaymentOptionsTest extends TestBase {
 		wait(5000);
 		click("viewBill_xpath");
 		wait(10000);
-		driver.findElement(By.xpath("//button[.='Close']")).click();
-		WebDriverWait wait=new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("makeAPayment_xpath")));
-
 		
-		click("makeAPayment_xpath");
+		try {
 
-		wait(10000);
+			Assert.assertTrue(isElementPresent("makeAPayment_xpath"),
+					"Make a payment button is not visible");
+
+		} catch (Throwable t) {
+
+			ErrorUtil.addVerificationFailure(t);
+
+		}
+		quit();
 
 	}
 

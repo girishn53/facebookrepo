beb19af329d9be862d668666b12a36259859d807
Girish <girishn53@gmail.com>
2017-01-04 22:27:07 +0530

    made changes in viewBillTest and project.properties

--- src/test/java/com/ctliselenium/framework/datadriven/Billing/ViewBillTest.java
+++ src/test/java/com/ctliselenium/framework/datadriven/Billing/ViewBillTest.java
@@ -2,12 +2,14 @@ package com.ctliselenium.framework.datadriven.Billing;
 
 import java.util.Hashtable;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.ctliselenium.framework.datadriven.TestBase;
 import com.ctliselenium.framework.datadriven.util.Constants;
+import com.ctliselenium.framework.datadriven.util.ErrorUtil;
 import com.ctliselenium.framework.datadriven.util.TestDataProvider;
 
 public class ViewBillTest extends TestBase {
@@ -27,7 +29,25 @@ public class ViewBillTest extends TestBase {
 		mouseOver("billingTab_xpath", driver);
 		wait(5000);
 		click("viewBill_xpath");
-		wait(1000);
+		
+		waitTillInvisible("loadImage_xpath", driver, 10);
+		
+		
+		switchToFrame("_48_INSTANCE_4lB9JAqqpqKl_iframe");
+		
+		try {
+
+			Assert.assertTrue(isElementPresent("advancedSearch_xpath"), "Advanced Search Option is not visible");
+
+		}
+
+		catch (Throwable t) {
+
+			ErrorUtil.addVerificationFailure(t);
+			Assert.fail("Advanced Search Option is not visible");
+			
+
+		}
 
 	}
 

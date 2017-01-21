b8c122478565677086e77f3a95fd762446a0fb2d
AB48044 <AB48044@5CG6191Y8K.ctl.intranet>
2017-01-21 13:36:53 +0530

    created complete test case PaymentOptionsTest

--- src/test/java/com/ctliselenium/framework/datadriven/Billing/PaymentOptionsTest.java
+++ src/test/java/com/ctliselenium/framework/datadriven/Billing/PaymentOptionsTest.java
@@ -3,9 +3,11 @@ package com.ctliselenium.framework.datadriven.Billing;
 import java.util.Hashtable;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -26,26 +28,77 @@ public class PaymentOptionsTest extends TestBase {
 		// it checks the runmodes of a test at all levels like suite , data
 		validateRunmodes("PaymentOptionsTest", Constants.SECOND_SUITE,
 				table.get(Constants.RUNMODE_COL));
+
 		doDefaultLogin(table.get(Constants.BROWSER_COL));
 
 		wait(10000);
-		mouseOver("billingTab_xpath", driver);
-		wait(5000);
-		click("viewBill_xpath");
-		wait(10000);
-		
+
 		try {
 
-			Assert.assertTrue(isElementPresent("makeAPayment_xpath"),
-					"Make a payment button is not visible");
+			boolean maintainPopUpisThere = isElementPresent("maintainPopUp_xpath");
 
-		} catch (Throwable t) {
+			if (maintainPopUpisThere) {
+				System.out.println("maintainenance pop up is present");
 
-			ErrorUtil.addVerificationFailure(t);
+				click("goToControlCenter_xpath");// if this xpath is not found
+													// it will throw an
+													// ElementNotVisibleException
+													// and test will stop, so I
+													// put it in try block
+
+			}
 
 		}
-		quit();
+
+		catch (ElementNotVisibleException e) {
+
+			System.out
+					.println("goToControlCenter_xpath element not fould and in catch block");
+
+		}
+
+		finally {
+
+			mouseOver("billingTab_xpath", driver);
+			wait(5000);
+			click("viewBill_xpath");
+			wait(1000);
+			waitTillInvisible("loadImage_xpath", driver, 10);
+			switchToFrame("_48_INSTANCE_4lB9JAqqpqKl_iframe");
+			click("makeAPayment_xpath");
+
+			// test for autopay option
+
+			try {
+				Assert.assertTrue(isElementPresent("autopay_xpath"),
+						"Autopay option is not present");
+			} catch (Throwable t) {
+				ErrorUtil.addVerificationFailure(t);
+				System.out.println("Autopay option is not present");
+
+			}
+
+			// payByCreditcard_xpath
+
+			try {
+				Assert.assertTrue(isElementPresent("payByCreditcard_xpath"),
+						"pay by credit card option is not present");
+			} catch (Throwable t) {
+				ErrorUtil.addVerificationFailure(t);
+				System.out.println("pay by credit card option is not present");
+
+			}
+
+		}
 
 	}
 
+	@AfterTest
+
+	public void closeBrowser() {
+
+		quit();
+
+	}
+	
 }
